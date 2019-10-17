803e0d5f4e36e15a91ec2a163a43188f1fb8afcf
RodrigoSenna88 <rodrigosennasantos@hotmail.com>
2019-10-16 23:38:00 -0300

    método post

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (req,res) => {
   return res.json(projects);
 });
 
+app.post('/projects', (req, res) => {
+  const { id, title} = req.body;
+ 
+  const project = {
+     id,
+     title,
+     task: []
+     }
+   projects.push(project);
+
+   return res.json(project);
+  
+   });
 
 app.listen(3333);
