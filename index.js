965ad93a8f26132fed5d54d512d632782e3154ca
RodrigoSenna88 <rodrigosennasantos@hotmail.com>
2019-10-19 11:30:02 -0300

    finalizando método delete

--- index.js
+++ index.js
@@ -36,4 +36,14 @@ app.post('/projects', (req, res) => {
       return res.json(projects);
    });
 
+   app.delete('/projects/:id', (req, res) => {
+     const { id } = req.params;
+
+     const project = projects.find(p => p.id == id);
+
+     projects.splice(id, 1);
+
+     return res.json(projects);
+   });
+
 app.listen(3333);
